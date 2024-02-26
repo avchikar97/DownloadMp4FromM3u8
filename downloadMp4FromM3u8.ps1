4da5594f96fbeacf656ccb18b27732850bf4fbdf
avchikar97 <avchikar97@gmail.com>
2024-02-25 19:51:44 -0800

    Create downloadMp4FromM3u8.ps1

---
+++ downloadMp4FromM3u8.ps1
@@ -0,0 +1,18 @@
+﻿param (
+    [Parameter(Mandatory=$true)][string]$url,
+    [Parameter(Mandatory=$true)][string]$name,
+    [Parameter(Mandatory=$false)][string]$dir="D:\downloadMp4FromM3u8"
+)
+
+[string]$OUTPUT_DIR = "$dir"
+[string]$OUTPUT_PATH = "$OUTPUT_DIR\$name.mp4"
+
+If(!(Test-Path -PathType container $OUTPUT_DIR))
+{
+      New-Item -Force -ItemType Directory -Path $OUTPUT_DIR
+}
+
+ffmpeg -i "$url" -c copy -bsf:a aac_adtstoasc $OUTPUT_PATH
+
+Write-Host 
+Write-Host Output file is in $OUTPUT_PATH
