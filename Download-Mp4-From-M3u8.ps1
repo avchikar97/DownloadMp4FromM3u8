ee503555e383daf46b9ac4120c96271f2f6bf2d2
avchikar97 <14263594+avchikar97@users.noreply.github.com>
2024-12-28 02:31:20 -0600

    Add switch to script to specify using the same video codec as source

--- Download-Mp4-From-M3u8.ps1
+++ Download-Mp4-From-M3u8.ps1
@@ -1,6 +1,7 @@
 ﻿param (
     [Parameter(Mandatory=$true, HelpMessage=".m3u8 URL of the stream to download")][string]$url,
     [Parameter(Mandatory=$true, HelpMessage="Output filename (not including .mp4 extension)")][string]$name,
+    [Parameter(Mandatory=$false, HelpMessage="If specified, video will be the same codec. Otherwise ffmpeg will encode the video with x265 to save space. This will likely speed up download.")][switch]$SameAsSource,
     [Parameter(Mandatory=$false, HelpMessage="Output directory of the downloaded .mp4")][string]$dir=(Join-Path "$PSScriptRoot" "downloadMp4FromM3u8")
 )
 
@@ -13,7 +14,12 @@ if (Get-Command "ffmpeg.exe" -ErrorAction SilentlyContinue){
         New-Item -Force -ItemType Directory -Path $OUTPUT_DIR
     }
 
-    ffmpeg -i "$url" -c:v libx265 -c:a copy -c:s copy $OUTPUT_PATH
+    if($SameAsSource){
+        ffmpeg -i "$url" -c copy $OUTPUT_PATH
+    }
+    else{
+        ffmpeg -i "$url" -c:v libx265 -c:a copy -c:s copy $OUTPUT_PATH
+    }
 
     Write-Host 
     Write-Host "Output file is in $OUTPUT_PATH"
