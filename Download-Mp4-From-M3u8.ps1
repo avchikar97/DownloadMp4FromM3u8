486b0d0dc8ebf33fd8630c743a25d70ea0b81382
avchikar97 <14263594+avchikar97@users.noreply.github.com>
2024-12-28 02:37:39 -0600

    Uppercase params

--- Download-Mp4-From-M3u8.ps1
+++ Download-Mp4-From-M3u8.ps1
@@ -1,12 +1,12 @@
 ﻿param (
-    [Parameter(Mandatory=$true, HelpMessage=".m3u8 URL of the stream to download")][string]$url,
-    [Parameter(Mandatory=$true, HelpMessage="Output filename (not including .mp4 extension)")][string]$name,
+    [Parameter(Mandatory=$true, HelpMessage=".m3u8 URL of the stream to download")][string]$Url,
+    [Parameter(Mandatory=$true, HelpMessage="Output filename (not including .mp4 extension)")][string]$Name,
     [Parameter(Mandatory=$false, HelpMessage="If specified, video will be the same codec. Otherwise ffmpeg will encode the video with x265 to save space. This will likely speed up download.")][switch]$SameAsSource,
-    [Parameter(Mandatory=$false, HelpMessage="Output directory of the downloaded .mp4")][string]$dir=(Join-Path "$PSScriptRoot" "downloadMp4FromM3u8")
+    [Parameter(Mandatory=$false, HelpMessage="Output directory of the downloaded .mp4")][string]$Dir=(Join-Path "$PSScriptRoot" "downloadMp4FromM3u8")
 )
 
-[string]$OUTPUT_DIR = "$dir"
-[string]$OUTPUT_PATH = (Join-Path "$OUTPUT_DIR" "$name.mp4")
+[string]$OUTPUT_DIR = "$Dir"
+[string]$OUTPUT_PATH = (Join-Path $OUTPUT_DIR "$($Name).mp4")
 
 if (Get-Command "ffmpeg.exe" -ErrorAction SilentlyContinue){
     If(!(Test-Path -PathType container $OUTPUT_DIR))
@@ -15,20 +15,20 @@ if (Get-Command "ffmpeg.exe" -ErrorAction SilentlyContinue){
     }
 
     if($SameAsSource){
-        ffmpeg -i "$url" -c copy $OUTPUT_PATH
+        ffmpeg -i $Url -c copy $OUTPUT_PATH
     }
     else{
-        ffmpeg -i "$url" -c:v libx265 -c:a copy -c:s copy $OUTPUT_PATH
+        ffmpeg -i $Url -c:v libx265 -c:a copy -c:s copy $OUTPUT_PATH
     }
 
     Write-Host 
-    Write-Host "Output file is in $OUTPUT_PATH"
+    Write-Host "Output file is $OUTPUT_PATH"
 
     if($IsLinux){
-        xdg-open $dir
+        xdg-open $OUTPUT_DIR
     }
     elseif($IsWindows){
-        explorer $dir
+        explorer $OUTPUT_DIR
     }
 }
 else{
