711a72004f40399d831727c15a8bb33a7114bed4
avchikar97 <14263594+avchikar97@users.noreply.github.com>
2025-02-04 23:24:09 -0800

    Add option to transcode to AV1

--- Download-Mp4-From-M3u8.ps1
+++ Download-Mp4-From-M3u8.ps1
@@ -1,7 +1,7 @@
 ﻿param (
     [Parameter(Mandatory=$true, HelpMessage=".m3u8 URL of the stream to download")][string]$Url,
     [Parameter(Mandatory=$true, HelpMessage="Output filename (not including .mp4 extension)")][string]$Name,
-    [Parameter(Mandatory=$false, HelpMessage="If specified, video will be the same codec. Otherwise ffmpeg will encode the video with x265 to save space. This will likely speed up download and be much easier on the CPU.")][switch]$SameAsSource,
+    [Parameter(Mandatory=$false, HelpMessage="Codec of the output video")][string]$Codec="H.264",
     [Parameter(Mandatory=$false, HelpMessage="Output directory of the downloaded .mp4")][string]$Dir=(Join-Path "$PSScriptRoot" "downloadMp4FromM3u8")
 )
 
@@ -13,11 +13,14 @@ if (Get-Command "ffmpeg.exe" -ErrorAction SilentlyContinue){
         New-Item -Force -ItemType Directory -Path $Dir
     }
 
-    if($SameAsSource){
-        ffmpeg -i $Url -c copy $OUTPUT_PATH
+    if($Codec -eq "AV1"){
+        ffmpeg -i $Url -c:v libsvtav1 -crf 30 -preset 5 -c:a copy -c:s copy $OUTPUT_PATH
     }
-    else{
+    elseif($Codec -eq "H.265"){
         ffmpeg -i $Url -c:v libx265 -crf 24 -preset slow -c:a copy -c:s copy $OUTPUT_PATH
+    }
+    else{ # fall back to same as source (usually H.264 but whatever)
+        ffmpeg -i $Url -c copy $OUTPUT_PATH
     }
 
     Write-Host "`nOutput file is $OUTPUT_PATH"
