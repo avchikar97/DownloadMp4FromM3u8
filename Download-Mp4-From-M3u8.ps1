db93341e84947d435c30f8f40e7cdfcd07bfd5f9
avchikar97 <14263594+avchikar97@users.noreply.github.com>
2025-02-23 13:52:22 -0800

    Just a few params to make downloading from web stream easier

--- Download-Mp4-From-M3u8.ps1
+++ Download-Mp4-From-M3u8.ps1
@@ -1,4 +1,6 @@
-﻿param (
+﻿# Can download .m3u8, .mpd, any video online video file
+
+param (
     [Parameter(Mandatory=$true, HelpMessage=".m3u8 URL of the stream to download")][string]$Url,
     [Parameter(Mandatory=$true, HelpMessage="Output filename (not including .mp4 extension)")][string]$Name,
     [Parameter(Mandatory=$false, HelpMessage="Codec of the output video")][string]$Codec="H.264",
@@ -13,14 +15,22 @@ if (Get-Command "ffmpeg.exe" -ErrorAction SilentlyContinue){
         New-Item -Force -ItemType Directory -Path $Dir
     }
 
+    $standard_params = @(
+        "-http_persistent", "0",
+        "-reconnect", "1",
+        "-reconnect_at_eof", "1",
+        "-reconnect_streamed", "1",
+        "-reconnect_delay_max", "2"
+    )
+
     if($Codec -eq "AV1"){
-        ffmpeg -i $Url -c:v libsvtav1 -crf 30 -preset 5 -c:a copy -c:s copy $OUTPUT_PATH
+        ffmpeg -i $Url -c:v libsvtav1 -crf 30 -preset 5 $standard_params -c:a copy -c:s copy $OUTPUT_PATH
     }
     elseif($Codec -eq "H.265"){
-        ffmpeg -i $Url -c:v libx265 -crf 24 -preset slow -c:a copy -c:s copy $OUTPUT_PATH
+        ffmpeg -i $Url -c:v libx265 -crf 24 -preset slow $standard_params -c:a copy -c:s copy $OUTPUT_PATH
     }
     else{ # fall back to same as source (usually H.264 but whatever)
-        ffmpeg -i $Url -c copy $OUTPUT_PATH
+        ffmpeg -i $Url $standard_params -c copy $OUTPUT_PATH
     }
 
     Write-Host "`nOutput file is $OUTPUT_PATH"
